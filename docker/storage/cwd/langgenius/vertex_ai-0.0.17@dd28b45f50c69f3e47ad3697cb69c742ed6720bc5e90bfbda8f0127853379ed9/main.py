21eca1fc85f8bdc8b1b1bf24bc4a410f91902c83
“Yoshikazu <nishino.y@monakaya.com>
2025-06-07 13:22:16 +0900

    プラグインの取り込みに問題あり。

--- docker/storage/cwd/langgenius/vertex_ai-0.0.17@dd28b45f50c69f3e47ad3697cb69c742ed6720bc5e90bfbda8f0127853379ed9/main.py
+++
@@ -1,9 +0,0 @@
-import grpc.experimental.gevent
-from dify_plugin import Plugin, DifyPluginEnv
-
-grpc.experimental.gevent.init_gevent()
-
-plugin = Plugin(DifyPluginEnv(MAX_REQUEST_TIMEOUT=120))
-
-if __name__ == '__main__':
-    plugin.run()
